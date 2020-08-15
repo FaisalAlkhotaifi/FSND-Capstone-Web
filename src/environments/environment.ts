08d504efadc9a9f676095b9e1145e8e5de954fc3
Faisal Alkhotaifi <falkhotaifi@gmail.com>
2020-08-15 21:49:54 +0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,6 @@ export const environment = {
     url: 'falkhotaifi.us', // the auth0 domain prefix
     audience: 'castingAgency', // the audience set for the auth0 app
     clientId: '7Hb68nHByGHlvkkRBfOU0NbZQh3bwOCj', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:4200', // the base url of the running ionic application. 
+    callbackURL: 'https://casting-agnecy-web-fsnd-20.herokuapp.com/', // the base url of the running ionic application. 
   }
 };
